0c87d78700dc06436efcfa03960b3d1229ff2830
Scott Bailey <csb5t@virginia.edu>
2017-11-01 14:49:55 -0700

    Sketch out basic Timeline components

--- webpack/index.jsx
+++ webpack/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import MasterVideo from './components/MasterVideo';
 import Dance from './components/Dance';
+import Timeline from './components/Timeline';
 import store from './store';
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
     <div className="app">
       <MasterVideo videoUrl="https://www.dropbox.com/s/r1kcgknu22b6iao/hashitomi-kiri-dance-part1.mov?dl=1" />
       <Dance />
+      <Timeline />
     </div>
   </Provider>
 );
