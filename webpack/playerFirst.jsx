1ab200821e0dbd95800ee4fda1f7150073dd7f5a
Juan Pablo López <jlopezfranco@gmail.com>
2017-11-30 21:52:36 +0100

    feat(player-layout): Basic template for player layout

---
+++ webpack/playerFirst.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => (
+  <div>
+    component 1
+  </div>
+);
+
+render(<App />, document.getElementById("playerFirst"));
