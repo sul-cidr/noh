f7d3e0409a8bb76d3d560d6b681b034af49d85c5
Juan Pablo López <jlopezfranco@gmail.com>
2018-01-16 08:36:45 +0000

    feat(sidebar-styles): Refactored some old or unnecessary styles.

--- webpack/components/Acts.jsx
+++ webpack/components/Acts.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Acts = () => (
   <div className="acts-container">
-    <div className="tl__act" style={{ width: "35%" }}>
+    <div className="act" style={{ width: "35%" }}>
       First act (mae ba)
     </div>
-    <div className="tl__act" style={{ width: "65%" }}>
+    <div className="act" style={{ width: "65%" }}>
       Second act (nochi ba)
     </div>
   </div>
