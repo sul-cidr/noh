5656e7f5b1785f52c36f3917bcdfdbf17f64e3a8
Scott Bailey <csb5t@virginia.edu>
2018-01-31 13:23:43 -0800

    Generate beat cells from actual data

--- webpack/__tests__/score.spec.jsx
+++ webpack/__tests__/score.spec.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { shallow } from "enzyme";
+import phrases from "./__fixtures__/phrases.json";
 import Score from "../components/Score";
 
 describe("<Score>", () => {
   it("renders as expected", () => {
-    const component = shallow(<Score />);
+    const component = shallow(<Score phrases={phrases.phrases} />);
     expect(component).toMatchSnapshot();
   });
 });
