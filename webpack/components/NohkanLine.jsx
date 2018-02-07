2257ddb7af3483834c73458117b6110235dd54b7
Scott Bailey <csb5t@virginia.edu>
2018-02-07 10:03:15 -0800

    Add cells in between non-consecutive cells and at the end of the line for proper alignment

--- webpack/components/NohkanLine.jsx
+++ webpack/components/NohkanLine.jsx
@@ -6,16 +6,42 @@ const NohkanLine = props => {
   if (props.grid.length === 0) {
     return <CellNohkan text="" length={props.length} />;
   }
-  // Need to rebuild the grid array to account for starting at different points
-  // and ending before the end of the full length
   const data = props.grid;
-  console.log(props.grid);
-  console.log("original data", data);
   if (data[0].start !== 0) {
-    data.unshift({ length: data[0].start - 1, text: "", start: 0 });
+    if (data[0].start === 1) {
+      data.unshift({ length: 1, text: "", start: 0 });
+    } else {
+      data.unshift({ length: data[0].start - 1, text: "", start: 0 });
+    }
   }
-  console.log("modified data", data);
-  const nohkanCells = data.map((cell, idx) => (
+  const fullData = [];
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i].start === 0) {
+      fullData.push(data[i]);
+    } else if (data[i].start === data[i - 1].length + data[i - 1].start) {
+      fullData.push(data[i]);
+    } else if (data[i].start !== data[i - 1].length + data[i - 1].start) {
+      fullData.push({
+        length: data[i].start - (data[i - 1].start + data[i - 1].length),
+        text: "",
+        start: data[i - 1].start + data[i - 1].length
+      });
+      fullData.push(data[i]);
+    }
+  }
+  let sumLengths = 0;
+  const lastItem = fullData[fullData.length - 1];
+  for (let i = 0; i < fullData.length; i += 1) {
+    sumLengths += fullData[i].length;
+  }
+  if (sumLengths !== props.length) {
+    fullData.push({
+      length: props.length - sumLengths,
+      text: "",
+      start: lastItem.start + lastItem.length + 1
+    });
+  }
+  const nohkanCells = fullData.map((cell, idx) => (
     <CellNohkan
       text={cell.text}
       length={cell.length}
