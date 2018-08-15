fae47f3d846381a8be80ee755ab89f95a21efdbf
Javier de la Rosa <versae@gmail.com>
2018-08-15 18:55:03 +0200

    Fixes #203. Adding support for unmetered beat phrases in sections

--- webpack/components/CellBeat.jsx
+++ webpack/components/CellBeat.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CellBeat = props => (
-  <div className="cell cell--1 cell--beat">{props.beatText}</div>
+  <div
+    className={`cell cell--${props.length} cell--${
+      props.beatText.toLowerCase() === "unmetered" ? "unmetered" : "beat"
+    }`}
+  >
+    {props.beatText}
+  </div>
 );
 
 CellBeat.propTypes = {
-  beatText: PropTypes.string.isRequired
+  beatText: PropTypes.string.isRequired,
+  length: PropTypes.number
+};
+
+CellBeat.defaultProps = {
+  length: 1
 };
 
 export default CellBeat;
