bdaae3f424a68f01cdecd6c89cf4307a63eb4773
Javier de la Rosa <versae@gmail.com>
2018-09-20 10:55:51 -0700

    Fixes #232. Adding functionality for the prev and next sentence score control

--- webpack/section.jsx
+++ webpack/section.jsx
@@ -40,6 +40,7 @@ export default class App extends Component {
             isPrevSentenceOn={false}
             startTime={this.props.startTime}
             duration={this.props.duration}
+            phrases={this.props.phrases}
           />
         ]
       ) : (
