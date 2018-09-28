e82c3bf76f11615d75594b9ca2e044181e98d3a9
Javier de la Rosa <versae@gmail.com>
2018-09-28 16:37:52 -0700

    Fixes #233. Adding parsing for filters and their functionality

--- webpack/scripts/catalog.js
+++ webpack/scripts/catalog.js
@@ -1,50 +1,48 @@
-import axios from "axios";
 import fs from "fs";
-import mkdirp from "mkdirp";
 import path from "path";
-import Papa from "papaparse";
-import { convertSecondsToHhmmss } from "../utils";
 import { downloadCSV, parserConfig, logError } from "./parser.js";
 
-const DEFAULT_LAYOUT = "website";
-const DEFAULT_IMAGE = "/assets/images/shodan/example.jpg";
-
-export const main = (configPath, quiet) => {
+export const main = (configPath) => {
   if (!configPath) {
-    logError("Usage: parse [path/to/parser.json] [-q/--quiet]");
+    logError("Usage: parse [path/to/parser.json]");
   }
-  const promises = [];
   try {
     const { catalog } = JSON.parse(fs.readFileSync(configPath, "utf8"));
-    const shodans = {};
-    promises.push(
-      Promise.all(
-        catalog.filters.map(
-          filter => (filter.url ? downloadCSV(filter.url) : null)
-        )
-      ).then(responses => {
-        catalog.filters.map((filter, index) => {
-          const headerRows = responses[index].data
-            .slice(0, filter.headers)
-            .map(row => row.slice(2));
-          const headers = headerRows[0].map((_, i) =>
-            headerRows.reduce((p, _, j) => `${p}/${headerRows[j][i]}`, "")
-          );
-          responses[index].data.slice(filter.headers).forEach(row => {
-            const [fileName, name] = row.slice(0, 2);
-            shodans[`${fileName}/${name}`] = {
-              [[filter.type]]: row
-                .slice(2)
-                .map((cell, index) => (cell ? headers[index - 2] : null))
-                .filter(Boolean)
-            };
-          });
-        });
-        console.log(shodans);
-      })
-    );
-    console.log(shodans);
-    return promises;
+    return Promise.all(
+      catalog.filters.map(filter => downloadCSV(filter.url))
+    ).then(responses => {
+      const headers = {};
+      const shodans = {};
+      catalog.filters.map((filter, index) => {
+        const headerRows = responses[index].data
+          .slice(0, filter.headers)
+          .map(row => row.slice(2));
+        headers[[filter.type]] = headerRows[0].map((_, header) =>
+          headerRows.reduce((previousRow, _, row) => [...previousRow, headerRows[row][header]].filter(Boolean), "")
+        );
+        responses[index].data.slice(filter.headers).forEach(row => {
+          const [fileName, name] = row.slice(0, 2);
+          shodans[`${fileName}/${name}`] = [
+            ...(shodans[`${fileName}/${name}`] || []),
+            ...row
+              .slice(2)
+              .map((cell, index) => (cell ? headers[filter.type][index] : null))
+              .filter(Boolean)
+          ]
+        })
+      });
+      const filters = Object.entries(headers)
+        .map(([key, values]) => ({by: key, values}))
+      const cards = Object.entries(shodans)
+        .map(([key, values]) => ({slug: key.split("/")[0], name: key.split("/")[1], pills: values}))
+      return {filters, cards};
+    }).then(catalog => {
+      fs.writeFileSync(
+        path.join("src", "_data", "catalog.json"),
+        JSON.stringify(catalog, null, 2)
+      );
+      return catalog
+    })
   } catch (error) {
     logError(`Malformed config file ${configPath}`, error);
   }
@@ -56,7 +54,6 @@ if (!module.parent) {
   main(
     process.argv[2] && process.argv[2][0] !== "-"
       ? process.argv[2]
-      : parserConfig,
-    ["-q", "--quiet"].includes(process.argv.slice(-1)[0])
+      : parserConfig
   );
 }
