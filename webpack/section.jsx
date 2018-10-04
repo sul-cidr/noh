529231022d25d5e162107f5568b1866539e7f2cf
inefable <jlopezfranco@gmail.com>
2018-10-04 21:59:54 +0200

    Section moved to previous state. Added snapshot.

--- webpack/section.jsx
+++ webpack/section.jsx
@@ -17,7 +17,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isHighlightedTextOn: false,
+      isHighlightedTextOn: true,
       isShodanTimelineOn: false
     };
   }
