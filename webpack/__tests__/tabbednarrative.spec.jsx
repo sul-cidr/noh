71720889b22b3a7cbcdfdcec074cac2f8ac2e7bb
Scott Bailey <csb5t@virginia.edu>
2018-10-16 16:10:38 -0700

    Add back semicolons after Prettier change

--- webpack/__tests__/tabbednarrative.spec.jsx
+++ webpack/__tests__/tabbednarrative.spec.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import { shallow } from "enzyme"
-import TabbedNarrative from "../components/TabbedNarrative"
+import React from "react";
+import { shallow } from "enzyme";
+import TabbedNarrative from "../components/TabbedNarrative";
 
 describe("<TabbedNarrative>", () => {
   const tabbedAnalysis =
-    "<div title='section one with many words in title' class='tabbed-narrative'><p>Lorem ipsum dolor sit amet.</p></div><br /><div title='section two'><p>Different stuff</p></div>"
+    "<div title='section one with many words in title' class='tabbed-narrative'><p>Lorem ipsum dolor sit amet.</p></div><br /><div title='section two'><p>Different stuff</p></div>";
 
   it("renders as expected", () => {
-    const component = shallow(<TabbedNarrative narrative={tabbedAnalysis} />)
-    expect(component).toMatchSnapshot()
-  })
-})
+    const component = shallow(<TabbedNarrative narrative={tabbedAnalysis} />);
+    expect(component).toMatchSnapshot();
+  });
+});
