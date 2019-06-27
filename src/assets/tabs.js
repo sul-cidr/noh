303a34de485d9686546c1d53aa0272d7b059e408
Peter Broadwell <broadwell@stanford.edu>
2019-06-26 18:20:32 -0700

    Support multiple tab sets; don't rely on element IDs

--- src/assets/tabs.js
+++ src/assets/tabs.js
@@ -1,52 +1,58 @@
 function attachTabs() {
   var tabClass = "react-tabs__tab";
-  var lu = document.createElement("lu");
-  lu.classList.add(`${tabClass}-list`);
 
-  var sections = document.querySelectorAll("section.tabbed-narrative");
-  sections.forEach(function(section, idx) {
-    var li = `
-    <li class="${tabClass}"
-        data-tab="${section.id}"
-        role="tab"
-        aria-selected="false"
-        aria-disabled="false"
-        aria-controls="tabs-${idx + 1}"
-    >
-      ${section.title}
-    </li>`;
-    lu.innerHTML += li;
-  });
-  document.getElementById("tabs").appendChild(lu);
-  var lis = document.querySelectorAll(`li.${tabClass}`);
-  [].forEach.call(lis, function(li) {
-    li.addEventListener("click", function() {
-      lis.forEach(function(li_) {
-        li_.classList.remove(`${tabClass}--selected`);
+  var containers = document.querySelectorAll("div.tabs-container");
+  containers.forEach(function(container) {
+    var ul = document.createElement("ul");
+    ul.classList.add(`${tabClass}-list`);
+
+    var sections = container.querySelectorAll("section");
+    sections.forEach(function(section, idx) {
+      var li = document.createElement("li");
+      li.classList.add(tabClass);
+      li.setAttribute("role", "tab");
+      if (idx == 0) {
+        section.style.display = "block";
+        li.classList.add(`${tabClass}--selected`);
+        li.setAttribute("aria-selected", "true");
+      } else {
+        section.style.display = "none";
+        li.setAttribute("aria-selected", "false");
+      }
+      li.setAttribute("aria-controls", "tabs-" + (idx + 1));
+      li.appendChild(document.createTextNode(section.title));
+
+      li.addEventListener("click", function() {
+        var lis = ul.querySelectorAll(`li.${tabClass}`);
+        // Deactivate all of the tabs and content panes
+        lis.forEach(function(li_) {
+          li_.classList.remove(`${tabClass}--selected`);
+          li.setAttribute("aria-selected", "false");
+        });
+        sections.forEach(function(section_) {
+          section_.style.display = "none";
+        });
+        // (Re)activate the clicked tab and its content pane
+        li.classList.add(`${tabClass}--selected`);
+        li.setAttribute("aria-selected", "true");
+        section.style.display = "block";
       });
-      li.classList.add(`${tabClass}--selected`);
-      sections.forEach(function(section_) {
-        section_.style.display = "none";
-      });
-      document.querySelector(
-        `section#${li.getAttribute("data-tab")}`
-      ).style.display =
-        "block";
+      ul.appendChild(li);
     });
+    container.querySelector("div.wrapper").children[0].appendChild(ul);
   });
-  // Show contents of first tab by default
-  lis[0].click();
 }
 
-if (document.querySelector("#tabs")) {
-  // in case the document is already rendered
-  if (document.readyState != "loading") attachTabs();
-  // modern browsers
-  else if (document.addEventListener)
-    document.addEventListener("DOMContentLoaded", attachTabs);
-  // IE <= 8
-  else
-    document.attachEvent("onreadystatechange", function() {
-      if (document.readyState == "complete") attachTabs();
-    });
-}
+/* It would be preferable to remove this entire section and instead just make
+ * sure that tabs.js is being loaded after all of the HTML on any page on
+ * which it is used.
+ */
+if (document.readyState != "loading") attachTabs();
+// modern browsers
+else if (document.addEventListener)
+  document.addEventListener("DOMContentLoaded", attachTabs);
+// IE <= 8
+else
+  document.attachEvent("onreadystatechange", function() {
+    if (document.readyState == "complete") attachTabs();
+  });
