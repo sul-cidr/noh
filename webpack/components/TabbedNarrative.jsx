170b6a4bc4830daf46513050a6949f808ca38b81
Peter Broadwell <broadwell@stanford.edu>
2019-06-28 14:51:04 -0700

    Remove PassThrough module ref at linter's request

--- webpack/components/TabbedNarrative.jsx
+++ webpack/components/TabbedNarrative.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Markup } from "interweave";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { PassThrough } from "stream";
 
 class TabbedNarrative extends React.Component {
   constructor(props) {
