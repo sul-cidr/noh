2f53fe1c8ddbbcc1042c3f826d318a50a4e061ea
Simon Wiles <simonjwiles@gmail.com>
2019-07-04 21:05:06 -0700

    Convert `play.jsx` from functional to class-based component
    This will be necessary for what must follow.  The correct way to do this would be to have a modern functional component with hooks which is connected to the redux store and then to have the initial state assembled when the store is created, rather than having much of it passed it a component-creation time.  However, in the interest of not attempting to rewrite the entire application at this stage, this will afford an acceptable solution.

--- webpack/play.jsx
+++ webpack/play.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
@@ -14,69 +14,87 @@ import contents from "./contents";
 import store from "./store";
 import { convertTimeToSeconds } from "./utils";
 
-const App = props => (
-  <Provider store={store}>
-    <div className="app-container">
-      <aside className="sidebar sidebar--play">
-        <div className="sidebar__header">
-          <div className="sidebar__back-link">
-            <a href="/plays" title="Plays">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="7"
-                height="11"
-                viewBox="0 0 7 11"
-              >
-                <path
-                  fill="currentColor"
-                  d="M0.197894737,5.07381279 L5.31236842,0.193127854 C5.55894737,-0.0421917808 5.95894737,-0.0421917808 6.20552632,0.193127854 L6.80210526,0.762465753 C7.04842105,0.997534247 7.04868421,1.37826484 6.80315789,1.61383562 L2.74973684,5.5 L6.80289474,9.38641553 C7.04868421,9.6219863 7.04815789,10.0027169 6.80184211,10.2377854 L6.20526316,10.8071233 C5.95868421,11.0424429 5.55868421,11.0424429 5.31210526,10.8071233 L0.197894737,5.92618721 C-0.0486842105,5.69086758 -0.0486842105,5.30913242 0.197894737,5.07381279 Z"
-                />
-              </svg>
-              Back to plays
-            </a>
-          </div>
-          <h1>{props.title}</h1>
-        </div>
-        <div className="sidebar__container">
-          <Narrative narrative={props.narrative} />
-        </div>
-      </aside>
-      <main>
-        <div className="video-player">
-          <div className="video-container">
-            <MasterVideo
-              videoUrl={props.videoUrl}
-              startTime={props.startTime}
-              tracks={props.tracks}
-            />
-          </div>
-          <div className="timeline">
-            <div className="timeline__container">
-              <Acts
-                acts={props.acts}
-                duration={convertTimeToSeconds(props.videoDuration)}
-              />
-              <div className="shodan-map__container">
-                <TimelineIndicator
-                  startTime={10}
-                  currentTime={props.currentTime}
-                  duration={convertTimeToSeconds(props.videoDuration)}
-                />
-                <ShodanTimeline
-                  sections={props.sections}
-                  maxIntensity={props.maxIntensity}
-                  totalDuration={convertTimeToSeconds(props.videoDuration)}
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    const {
+      acts,
+      currentTime,
+      maxIntensity,
+      narrative,
+      sections,
+      startTime,
+      title,
+      tracks,
+      videoDuration,
+      videoUrl
+    } = this.props;
+
+    return (
+      <Provider store={store}>
+        <div className="app-container">
+          <aside className="sidebar sidebar--play">
+            <div className="sidebar__header">
+              <div className="sidebar__back-link">
+                <a href="/plays" title="Plays">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="7"
+                    height="11"
+                    viewBox="0 0 7 11"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M0.197894737,5.07381279 L5.31236842,0.193127854 C5.55894737,-0.0421917808 5.95894737,-0.0421917808 6.20552632,0.193127854 L6.80210526,0.762465753 C7.04842105,0.997534247 7.04868421,1.37826484 6.80315789,1.61383562 L2.74973684,5.5 L6.80289474,9.38641553 C7.04868421,9.6219863 7.04815789,10.0027169 6.80184211,10.2377854 L6.20526316,10.8071233 C5.95868421,11.0424429 5.55868421,11.0424429 5.31210526,10.8071233 L0.197894737,5.92618721 C-0.0486842105,5.69086758 -0.0486842105,5.30913242 0.197894737,5.07381279 Z"
+                    />
+                  </svg>
+                  Back to plays
+                </a>
+              </div>
+              <h1>{title}</h1>
+            </div>
+            <div className="sidebar__container">
+              <Narrative narrative={narrative} />
+            </div>
+          </aside>
+          <main>
+            <div className="video-player">
+              <div className="video-container">
+                <MasterVideo
+                  videoUrl={videoUrl}
+                  startTime={startTime}
+                  tracks={tracks}
                 />
               </div>
+              <div className="timeline">
+                <div className="timeline__container">
+                  <Acts
+                    acts={acts}
+                    duration={convertTimeToSeconds(videoDuration)}
+                  />
+                  <div className="shodan-map__container">
+                    <TimelineIndicator
+                      startTime={10}
+                      currentTime={currentTime}
+                      duration={convertTimeToSeconds(videoDuration)}
+                    />
+                    <ShodanTimeline
+                      sections={sections}
+                      maxIntensity={maxIntensity}
+                      totalDuration={convertTimeToSeconds(videoDuration)}
+                    />
+                  </div>
+                </div>
+                {/* These have to update based on current time */}
+                <IntermediaTable play={title} sections={sections} />
+              </div>
             </div>
-            {/* These have to update based on current time */}
-            <IntermediaTable play={props.title} sections={props.sections} />
-          </div>
+          </main>
         </div>
-      </main>
-    </div>
-  </Provider>
-);
+      </Provider>
+    );
+  }
+}
 
 App.propTypes = {
   title: PropTypes.string,
@@ -149,5 +167,3 @@ if (!module.parent) {
     render(<App {...props} />, document.getElementById("play"));
   });
 }
-
-export default App;
