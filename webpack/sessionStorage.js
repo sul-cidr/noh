649d330c67ede2c7cf8bb4e04d30a389d26701a2
Simon Wiles <simonjwiles@gmail.com>
2019-07-04 21:08:07 -0700

    Add sessionStorage handler

---
+++ webpack/sessionStorage.js
@@ -0,0 +1,21 @@
+export const loadState = () => {
+  // returning undefined will cause redux to use the default values
+  try {
+    const serializedState = sessionStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    sessionStorage.setItem("state", serializedState);
+  } catch (err) {
+    // write error
+  }
+};
--- webpack/store.js
+++ webpack/store.js
@@ -2,11 +2,17 @@
 import { createStore } from "redux";
 import reducer, { DEFAULT_STATE } from "./reducers";
 import { reduxDevTools } from "./utils";
-import { loadState } from "./localStorage";
+import { loadState as loadStateFromLocalStorage } from "./localStorage";
+import { loadState as loadStateFromSessionStorage } from "./sessionStorage";
 
 const store = createStore(
   reducer,
-  Object.assign({}, DEFAULT_STATE, loadState()),
+  Object.assign(
+    {},
+    DEFAULT_STATE,
+    loadStateFromLocalStorage(),
+    loadStateFromSessionStorage()
+  ),
   reduxDevTools()
 );
 
