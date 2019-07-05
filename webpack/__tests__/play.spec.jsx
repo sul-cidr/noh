109ed8601a99ec4dcfe92d6540a53f1767afea58
Simon Wiles <simonjwiles@gmail.com>
2019-07-05 00:16:48 -0700

    Extend test coverage for `play.jsx`

--- webpack/__tests__/play.spec.jsx
+++ webpack/__tests__/play.spec.jsx
@@ -4,20 +4,24 @@ import configureMockStore from "redux-mock-store";
 
 import App from "../play";
 import fixtures from "./__fixtures__/play.json";
+import { DEFAULT_STATE } from "../reducers";
 
 describe("<Play>", () => {
   let wrapper;
   let store;
 
   beforeEach(() => {
-    const initialState = {};
     const mockStore = configureMockStore();
 
-    store = mockStore(initialState);
+    store = mockStore(DEFAULT_STATE);
     wrapper = mount(<App store={store} {...fixtures} />);
   });
 
   it("renders as expected", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("starts at 0 if no valid origin is loaded from sessionStorage", () => {
+    expect(store.getState().currentTime.time).toBe(0);
   });
 });
