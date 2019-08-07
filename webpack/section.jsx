e9e6bac4285c358e229d103bfa35de05e23f3781
Simon Wiles <simonjwiles@gmail.com>
2019-08-06 17:01:44 -0700

    Show arrow in "closed" state when no libretto is available
    Make `isHighlightedTextOn` defaults to `true` only in the presence of a libretto to show.

--- webpack/section.jsx
+++ webpack/section.jsx
@@ -32,7 +32,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isHighlightedTextOn: true,
+      isHighlightedTextOn: props.captions.length > 0,
       isShodanTimelineOn: false
     };
 
