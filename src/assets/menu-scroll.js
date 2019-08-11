3ae08c531a41ae7eaa9e4fb286fd88943290e9e7
Peter Broadwell <broadwell@stanford.edu>
2019-08-11 12:22:24 -0700

    Smooth show/hide transitions

--- src/assets/menu-scroll.js
+++ src/assets/menu-scroll.js
@@ -1,26 +1,33 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {
   headerShowHide();
 };
 
-// Get the header
 var header = document.querySelector("nav.second-level-menu__container");
 
-// Get the offset position of the navbar
+// Get the offset position and height of the navbar
 var sticky = header.offsetTop;
+var headerHeight = header.offsetHeight;
 
 var prevScrollPos = window.pageYOffset;
 function headerShowHide() {
   var currentScrollPos = window.pageYOffset;
 
-  if (prevScrollPos > currentScrollPos && currentScrollPos > sticky) {
-    //header.classList.remove("sticky-hidden");
-    header.classList.add("sticky");
+  // Scrolling up
+  if (prevScrollPos > currentScrollPos) {
+    if (currentScrollPos > sticky) {
+      // Slide out floating header if we're not near the page top
+      header.classList.remove("sticky-hidden");
+      header.classList.add("sticky");
+    } else {
+      // If we scroll into the page top, let header "dock" normally
+      header.classList.remove("sticky");
+    }
+    // Scrolling down
   } else {
-    //if (currentScrollPos > sticky) {
-    header.classList.remove("sticky");
-    //header.classList.add("sticky-hidden");
-    //}
+    // Slide in (hide) the header if we're scrolling down in the page middle
+    if (currentScrollPos > sticky + headerHeight) {
+      header.classList.add("sticky-hidden");
+    }
   }
   prevScrollPos = currentScrollPos;
 }
