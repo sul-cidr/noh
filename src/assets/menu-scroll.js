3b8f369689660152b6d702f65ec5e25029515724
Simon Wiles <simonjwiles@gmail.com>
2019-08-12 16:10:00 -0700

    Reimplement behaviour to prevent jerky docking

--- src/assets/menu-scroll.js
+++ src/assets/menu-scroll.js
@@ -1,33 +1,38 @@
-window.onscroll = function() {
-  headerShowHide();
-};
-
-var header = document.querySelector("nav.second-level-menu__container");
-
+var secondLevelMenu = document.querySelector(
+  "nav.second-level-menu__container"
+);
 // Get the offset position and height of the navbar
-var sticky = header.offsetTop;
-var headerHeight = header.offsetHeight;
+var menuTop = secondLevelMenu.offsetTop;
+var menuHeight = secondLevelMenu.offsetHeight;
+
+var stickyMenu = secondLevelMenu.cloneNode((deep = true));
+stickyMenu.classList.add("sticky");
+document.body.append(stickyMenu);
 
 var prevScrollPos = window.pageYOffset;
+
 function headerShowHide() {
   var currentScrollPos = window.pageYOffset;
 
   // Scrolling up
   if (prevScrollPos > currentScrollPos) {
-    if (currentScrollPos > sticky) {
+    if (currentScrollPos > menuTop) {
       // Slide out floating header if we're not near the page top
-      header.classList.remove("sticky-hidden");
-      header.classList.add("sticky");
+      stickyMenu.classList.add("visible");
     } else {
       // If we scroll into the page top, let header "dock" normally
-      header.classList.remove("sticky");
+      stickyMenu.setAttribute("style", "display: none");
+      stickyMenu.classList.remove("visible");
+      setTimeout(() => stickyMenu.setAttribute("style", "display: block"), 210);
     }
     // Scrolling down
   } else {
     // Slide in (hide) the header if we're scrolling down in the page middle
-    if (currentScrollPos > sticky + headerHeight) {
-      header.classList.add("sticky-hidden");
+    if (currentScrollPos > menuTop + menuHeight) {
+      stickyMenu.classList.remove("visible");
     }
   }
   prevScrollPos = currentScrollPos;
 }
+
+window.addEventListener("scroll", headerShowHide);
