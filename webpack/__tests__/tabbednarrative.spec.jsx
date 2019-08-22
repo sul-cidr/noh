b7e349d1065181851512699817befd664b6418d8
Simon Wiles <simonjwiles@gmail.com>
2019-08-22 12:40:44 -0700

    Update tests and snapshots

--- webpack/__tests__/tabbednarrative.spec.jsx
+++ webpack/__tests__/tabbednarrative.spec.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-import TabbedNarrative from "../components/TabbedNarrative";
+import { Unwrapped as TabbedNarrative } from "../components/TabbedNarrative";
 
 describe("<TabbedNarrative>", () => {
   const tabbedAnalysis =
     "<div title='section one with many words in title' class='tabbed-narrative'><p>Lorem ipsum dolor sit amet.</p></div><br /><div title='section two'><p>Different stuff</p></div>";
 
   it("renders as expected", () => {
-    const component = shallow(<TabbedNarrative narrative={tabbedAnalysis} />);
+    const component = shallow(
+      <TabbedNarrative
+        narrative={tabbedAnalysis}
+        narrativeTab={0}
+        updateNarrativeTab={() => jest.fn()}
+      />
+    );
     expect(component).toMatchSnapshot();
   });
 
   it("ref handler runs with tabs ref", () => {
     const component = mount(
       <div className="narrative" role="presentation">
-        <TabbedNarrative narrative={tabbedAnalysis} />
+        <TabbedNarrative
+          narrative={tabbedAnalysis}
+          narrativeTab={0}
+          updateNarrativeTab={() => jest.fn()}
+        />
       </div>
     );
     const tabN = component.find("TabbedNarrative");
@@ -22,7 +32,13 @@ describe("<TabbedNarrative>", () => {
   });
 
   it("ref handler runs with null input", () => {
-    const component = shallow(<TabbedNarrative narrative={tabbedAnalysis} />);
+    const component = shallow(
+      <TabbedNarrative
+        narrative={tabbedAnalysis}
+        narrativeTab={0}
+        updateNarrativeTab={() => jest.fn()}
+      />
+    );
     component.instance().handleDomRef(null);
   });
 });
