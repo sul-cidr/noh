a7c02409a89706321bea545c0521e0f7e5e517b6
Simon Wiles <simonjwiles@gmail.com>
2019-08-29 16:13:46 -0700

    Update fixture and add new test

--- webpack/__tests__/play.spec.jsx
+++ webpack/__tests__/play.spec.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { mount } from "enzyme";
+import TestUtils from "react-dom/test-utils";
 
 import App from "../play";
 import fixtures from "./__fixtures__/play.json";
@@ -34,5 +35,38 @@ describe("<Play>", () => {
       time: 1000,
       origin: "URL_FRAG"
     });
+  });
+});
+
+describe("", () => {
+  it("", () => {
+    const component = TestUtils.renderIntoDocument(<App {...fixtures} />);
+
+    const sectionTitle = TestUtils.findAllInRenderedTree(
+      component,
+      inst =>
+        TestUtils.isDOMComponent(inst) &&
+        inst.attributes["data-highlight-dans"] !== undefined
+    );
+
+    const shodanBlock = TestUtils.findAllInRenderedTree(
+      component,
+      inst =>
+        TestUtils.isDOMComponent(inst) &&
+        inst.attributes["data-index"] !== undefined &&
+        inst.attributes["data-index"].value === "1"
+    );
+
+    expect(
+      shodanBlock[0].classList.contains("shodan-map__item--highlight")
+    ).toBe(false);
+    sectionTitle[0].dispatchEvent(new Event("mouseover"));
+    expect(
+      shodanBlock[0].classList.contains("shodan-map__item--highlight")
+    ).toBe(true);
+    sectionTitle[0].dispatchEvent(new Event("mouseout"));
+    expect(
+      shodanBlock[0].classList.contains("shodan-map__item--highlight")
+    ).toBe(false);
   });
 });
