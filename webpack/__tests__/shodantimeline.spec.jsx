d70017d5a65913177b17a4cac66b1321a68aa0c0
Simon Wiles <simonjwiles@gmail.com>
2020-04-09 22:44:21 -0700

    Load full fixtures into <ShodanTimeline/> test, and use the needed bits

--- webpack/__tests__/shodantimeline.spec.jsx
+++ webpack/__tests__/shodantimeline.spec.jsx
@@ -2,29 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import ShodanTimeline from "../components/ShodanTimeline";
 
+import fixtures from "./__fixtures__/play.json";
+
 describe("<ShodanTimeline>", () => {
-  const sections = [
-    {
-      sectionName: { value: "Noriji" },
-      sectionUrl: "/noriji",
-      intensity: { number: "17" },
-      startTime: { value: 0 },
-      endTime: { value: 300 }
-    },
-    {
-      sectionName: { value: "Maibataraki" },
-      sectionUrl: "/maibataraki",
-      intensity: { number: "12" },
-      startTime: { value: 301 },
-      endTime: { value: 1500 }
-    }
-  ];
   it("renders as expected", () => {
     const component = shallow(
       <ShodanTimeline
-        sections={sections}
+        sections={fixtures.sections}
         maxIntensity={21}
         totalDuration={1800}
+        acts={fixtures.acts}
       />
     );
     expect(component).toMatchSnapshot();
