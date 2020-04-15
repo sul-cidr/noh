d4c1727bf0608fd77a11ee49dce6874d09c64617
Juan Pablo Lopez Franco <jlopezfranco@gmail.com>
2020-04-15 22:41:00 +0200

    Added link to catalog on Level 2.

--- webpack/section.jsx
+++ webpack/section.jsx
@@ -137,23 +137,33 @@ export default class App extends Component {
         phrases={this.props.phrases}
       />
     );
-    const shodanName = this.props.shodanType.value ? (
-      <span>
+    const shodanLink = this.props.shodanType.value && (
+      <div className="sidebar__catalog-link">
         <a
-          className="shodan__outlink"
           href={`/catalog-of-shodan/${this.props.shodanType.value}`}
-          title="Definition"
+          title={this.props.playName}
           target="_blank"
           rel="noopener noreferrer"
         >
-          {this.props.title}{" "}
-          <sup className="shodan__outlink-circlei">
-            <i className="fas fa-info-circle" />
-          </sup>
+          In catalog of Shōdan
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="th"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="svg-inline--fa fa-th fa-w-16 fa-3x"
+          >
+            <path
+              fill="currentColor"
+              d="M149.333 56v80c0 13.255-10.745 24-24 24H24c-13.255 0-24-10.745-24-24V56c0-13.255 10.745-24 24-24h101.333c13.255 0 24 10.745 24 24zm181.334 240v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.256 0 24.001-10.745 24.001-24zm32-240v80c0 13.255 10.745 24 24 24H488c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24zm-32 80V56c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.256 0 24.001-10.745 24.001-24zm-205.334 56H24c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24zM0 376v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 24zm386.667-56H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zm0 160H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H386.667c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zM181.333 376v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24z"
+              class=""
+            ></path>
+          </svg>
         </a>
-      </span>
-    ) : (
-      this.props.title
+      </div>
     );
     const toggle = (
       <svg
@@ -189,9 +199,8 @@ export default class App extends Component {
                   {this.props.playName}
                 </a>
               </div>
-              <h1 data-intro="This link opens a new tab with the Shōdan's associated definition page.">
-                {shodanName}
-              </h1>
+              <h1>{this.props.title}</h1>
+              {shodanLink}
             </div>
             <div
               className="sidebar__container"
